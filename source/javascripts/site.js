d1fd414243a115800b3943a28879890b45ef20f3
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2021-01-04 16:51:18 +0100

    nav links

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,16 +1,17 @@
 window.onscroll = () => {
   const nav = document.querySelector("#nb");
   const navbrand = nav.querySelector("a");
-  const navlink = nav.querySelectorAll("li.nav-item > a");
+  const navlinks = nav.querySelectorAll("li.nav-item > a");
+  console.log(navlinks);
   if (this.scrollY <= 10) {
     nav.className =
       "navbar navbar-expand-lg fixed-top navbar-light bg-light p-3 background-invisible";
-    navlink.className = "nav-link color-linen";
+    navlinks.forEach((link) => (link.className = "nav-link color-linen"));
     navbrand.className = "navbar-brand color-linen";
   } else {
     navbrand.className = "navbar-brand color-dark";
     nav.className =
       "navbar navbar-expand-lg fixed-top navbar-light bg-light p-3 scroll";
-    navlink.className = "nav-link color-dark";
+    navlinks.forEach((link) => (link.className = "nav-link color-dark"));
   }
 };
