2f55906171d3ccbd47cab7397ec486d3d729338c
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2021-01-04 15:42:19 +0100

    changing navbar

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,9 +1,16 @@
 window.onscroll = () => {
   const nav = document.querySelector("#nb");
-  if (this.scrollY <= 10)
+  const navbrand = nav.querySelector("a");
+  const navlink = nav.querySelectorAll("li.nav-item > a");
+  if (this.scrollY <= 10) {
     nav.className =
       "navbar navbar-expand-lg fixed-top navbar-light bg-light p-3 background-invisible";
-  else
+    navlink.className = "nav-link color-linen";
+    navbrand.className = "navbar-brand color-linen";
+  } else {
+    navbrand.className = "navbar-brand color-dark";
     nav.className =
       "navbar navbar-expand-lg fixed-top navbar-light bg-light p-3 scroll";
+    navlink.className = "nav-link color-dark";
+  }
 };
