f98dab959a26ad0966210529543ee9a2eba16050
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2021-01-19 20:06:17 +0100

    navbar and header v1

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,17 +1,17 @@
-window.onscroll = () => {
-  const nav = document.querySelector("#nb");
-  const navbrand = nav.querySelector("a");
-  const navlinks = nav.querySelectorAll("li.nav-item > a");
-  console.log(navlinks);
-  if (this.scrollY <= 10) {
-    nav.className =
-      "navbar navbar-expand-lg fixed-top navbar-light bg-light p-3 background-invisible";
-    navlinks.forEach((link) => (link.className = "nav-link color-linen"));
-    navbrand.className = "navbar-brand color-linen";
-  } else {
-    navbrand.className = "navbar-brand color-dark";
-    nav.className =
-      "navbar navbar-expand-lg fixed-top navbar-light bg-light p-3 scroll";
-    navlinks.forEach((link) => (link.className = "nav-link color-dark"));
-  }
-};
+// window.onscroll = () => {
+//   const nav = document.querySelector("#nb");
+//   const navbrand = nav.querySelector("a");
+//   const navlinks = nav.querySelectorAll("li.nav-item > a");
+//   console.log(navlinks);
+//   if (this.scrollY <= 10) {
+//     nav.className =
+//       "navbar navbar-expand-lg fixed-top navbar-light bg-light p-3 background-invisible";
+//     navlinks.forEach((link) => (link.className = "nav-link color-linen"));
+//     navbrand.className = "navbar-brand color-linen";
+//   } else {
+//     navbrand.className = "navbar-brand color-dark";
+//     nav.className =
+//       "navbar navbar-expand-lg fixed-top navbar-light bg-light p-3 scroll";
+//     navlinks.forEach((link) => (link.className = "nav-link color-dark"));
+//   }
+// };
