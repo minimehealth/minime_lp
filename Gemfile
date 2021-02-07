8bbed8cb0cb58112927d8980e655c2cbd06b62cf
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2021-02-07 10:52:15 +0100

    sync s3 bucket setup

--- Gemfile
+++ Gemfile
@@ -1,7 +1,29 @@
 source 'https://rubygems.org'
 
+# Specify your gem's dependencies in middleman-aws.gemspec
+# gemspec
+
 gem 'middleman', '~> 4.2'
 gem 'middleman-autoprefixer', '~> 2.7'
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby, :x64_mingw]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw, :x64_mingw]
 gem 'middleman-livereload'
+
+# Deployment - requires additional configuration.  See links below
+gem 'middleman-s3_sync', '~> 4.0.1.rc.3'
+gem 'mime-types', '~> 3.0.0'
+# gem 'middleman-aws'         # https://github.com/alienfast/middleman-aws
+
+# # Even though already required by the middleman-aws gem, it appears middleman does not
+# #   pick up transitive dependency extensions early enough  to avoid the
+# #   "== Unknown Extension:" error.  Add these to your main project
+# #   (I wish this was unnecessary but don't know how to work around it)
+
+# # https://github.com/fredjean/middleman-s3_sync
+# gem 'middleman-s3_sync', '>=4.0.0'
+
+# https://github.com/andrusha/middleman-cloudfront
+# gem "middleman-cloudfront"
+
+#------------------------------------------------------------------------
+# Other dependencies below here.
--- config.rb
+++ config.rb
@@ -47,13 +47,56 @@ page '/*.txt', layout: false
 #   end
 # end
 
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                = 'minimewebsite'
+  s3_sync.region                = 'eu-central-1'
+  s3_sync.aws_access_key_id     = ENV["AWS_ACCESS_KEY_ID"]
+  s3_sync.aws_secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
+end
+
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
+
+# #------------------------------------------------------------------------
+# # Configuration variables specific to each project
+# #------------------------------------------------------------------------
+# SITE_NAME                       = 'minime'
+# URL_ROOT                        = 'https://minime.health'
+# AWS_BUCKET                      = 'minime-lp'
+# AWS_CLOUDFRONT_DISTRIBUTION_ID  = 'xxxxxxxxxxxx'
+
+# activate :directory_indexes  #Removes .html from file
+
+# AWS_ACCESS_KEY= ENV['AWS_ACCESS_KEY']
+# AWS_SECRET= ENV['AWS_SECRET']
+
+# # https://github.com/fredjean/middleman-s3_sync
+# activate :s3_sync do |s3_sync|
+#   s3_sync.bucket                     = AWS_BUCKET # The name of the S3 bucket you are targeting. This is globally unique.
+#   # s3_sync.region                     = 'us-east-1'     # The AWS region for your bucket. (S3 no longer requires this, dummy input?)
+#   s3_sync.aws_access_key_id          = AWS_ACCESS_KEY
+#   s3_sync.aws_secret_access_key      = AWS_SECRET
+#   s3_sync.delete                     = false # We delete stray files by default.
+#   # s3_sync.after_build                = false # We do not chain after the build step by default.
+#   # s3_sync.prefer_gzip                = true
+#   # s3_sync.path_style                 = true
+#   # s3_sync.reduced_redundancy_storage = false
+#   # s3_sync.acl                        = 'public-read'
+#   # s3_sync.encryption                 = false
+#   # s3_sync.prefix                     = ''
+#   # s3_sync.version_bucket             = false
 # end
 
-activate :directory_indexes  #Removes .html from file
+# # https://github.com/andrusha/middleman-cloudfront
+# activate :cloudfront do |cf|
+#   cf.access_key_id                    = AWS_ACCESS_KEY
+#   cf.secret_access_key                = AWS_SECRET
+#   cf.distribution_id                  = AWS_CLOUDFRONT_DISTRIBUTION_ID
+#   # cf.filter = /\.html$/i
+# end
 
