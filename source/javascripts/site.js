11d77e047750e7e8c57b82953619d3c74c2c6b59
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2022-09-01 13:19:49 +0200

    patients page done

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -6,6 +6,22 @@ function offset(el) {
 }
 
 
+function copyToClipboard() {
+  /* Get the text field */
+  let copyText = document.getElementById("websiteLink");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+  /* Alert the copied text */
+  // alert("Copied the text: " + copyText.value);
+}
+
+
 // document.addEventListener('scroll', function (e) {
 //   // const appsteps = document.getElementById("app-steps")
 //   const how = document.getElementById("testi")
