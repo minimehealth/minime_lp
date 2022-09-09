b3861762555a60eb6a85377b51f398aa31860ef6
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2022-09-09 19:02:27 +0200

    test js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -4,21 +4,21 @@ function offset(el) {
     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
 }
+//
 
+function copyToClipboard() {
+  /* Get the text field */
+  let copyText = document.getElementById("websiteLink");
 
-// function copyToClipboard() {
-//   /* Get the text field */
-//   let copyText = document.getElementById("websiteLink");
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-//   /* Select the text field */
-//   copyText.select();
-//   copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
 
-//   /* Copy the text inside the text field */
-//   navigator.clipboard.writeText(copyText.value);
-
-//   /* Alert the copied text */
-//   // alert("Copied the text: " + copyText.value);
-// }
+  /* Alert the copied text */
+  // alert("Copied the text: " + copyText.value);
+}
 
 //= require gdpr/cookie_consent
