54681b110ec919368ee26dccd92e979dfe8b230e
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2022-09-13 12:22:39 +0200

    js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,24 +1,24 @@
-function offset(el) {
-  var rect = el.getBoundingClientRect(),
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-}
+// function offset(el) {
+//   var rect = el.getBoundingClientRect(),
+//     scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+//     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//   return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+// }
 //
 
-// function copyToClipboard() {
-//   /* Get the text field */
-//   let copyText = document.getElementById("websiteLink");
+function copyToClipboard() {
+  /* Get the text field */
+  let copyText = document.getElementById("websiteLink");
 
-//   /* Select the text field */
-//   copyText.select();
-//   copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-//   /* Copy the text inside the text field */
-//   navigator.clipboard.writeText(copyText.value);
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
 
-//   /* Alert the copied text */
-//   // alert("Copied the text: " + copyText.value);
-// }
+  /* Alert the copied text */
+  // alert("Copied the text: " + copyText.value);
+}
 
 //= require gdpr/cookie_consent
